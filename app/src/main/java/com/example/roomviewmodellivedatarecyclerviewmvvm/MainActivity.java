af80b9e4398c4ef838de9f6f270df65ac666f22d
ramimhossain12 <ramimhossain6242@gmail.com>
2020-08-21 20:33:27 +0600

    recyclerView and Adapter set

--- app/src/main/java/com/example/roomviewmodellivedatarecyclerviewmvvm/MainActivity.java
+++ app/src/main/java/com/example/roomviewmodellivedatarecyclerviewmvvm/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.roomviewmodellivedatarecyclerviewmvvm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,13 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdaoter adaoter = new NoteAdaoter();
+        recyclerView.setAdapter(adaoter);
+
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                ///........updater recyclearView
 
-                Toast.makeText(MainActivity.this,"OnChange",Toast.LENGTH_SHORT).show();
+
+                adaoter.setNotes(notes);
 
 
             }
